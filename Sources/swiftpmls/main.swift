cf9bff01fb181be50336c2280a394516ece05274
Leo Dion <leogdion@brightdigit.com>
2020-04-09 11:17:52 -0400

    migrating to struct

--- Sources/swiftpmls/main.swift
+++ Sources/swiftpmls/main.swift
@@ -1,1 +1,3 @@
-print("hello world!")
+import PackageListValidator
+
+Validator.main()
--- Tests/PackageListValidatorTests/PackageListValidatorTests.swift
+++ Tests/PackageListValidatorTests/PackageListValidatorTests.swift
@@ -6,7 +6,7 @@ final class PackageListValidatorTests: XCTestCase {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct
     // results.
-    XCTAssertEqual(PackageListValidator().text, "Hello, World!")
+    // XCTAssertEqual(PackageListValidator().text, "Hello, World!")
   }
 
   static var allTests = [
