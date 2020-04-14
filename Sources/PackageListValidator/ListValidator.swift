856fd741eb5528026d3a77fccd4c8f9ea824a0a9
Leo Dion <leogdion@brightdigit.com>
2020-04-14 12:49:54 -0400

    fixed validation

--- Sources/PackageListValidator/ListValidator.swift
+++ Sources/PackageListValidator/ListValidator.swift
@@ -28,6 +28,6 @@ extension ListValidator {
   }
 
   func validateUrls(_ urls: [URL]) -> ValidationError? {
-    ValidationError(description: Self.errorDescription, urls: urls)
+    ValidationError(description: Self.errorDescription, urls: self.invalidUrls(fromUrls: urls))
   }
 }
