a1e8fc0af5fe8b3d1885cbb77318c881c4d396f0
Leo Dion <leogdion@brightdigit.com>
2020-04-14 19:19:12 -0400

    remove old debug statements

--- Sources/PackageListValidator/SwiftPackageReporter.swift
+++ Sources/PackageListValidator/SwiftPackageReporter.swift
@@ -15,10 +15,8 @@ public struct SwiftPackageReporter {
     }.then { downloadURL in
       self.parser.verifyPackageDump(at: downloadURL, withDecoder: decoder)
     }.map { detail in
-      debugPrint("Verified \(gitURL)")
       return SwiftPackageReport(url: gitURL, result: .success(detail))
     }.recover(only: PackageError.self) { error -> Guarantee<SwiftPackageReport> in
-      debugPrint("Failed \(gitURL): \(error.friendlyName)")
       return Guarantee {
         $0(SwiftPackageReport(url: gitURL, result: .failure(error)))
       }
